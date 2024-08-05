153291f2c9cb0c557d34ca5a67fdf121839a1a60
Don <496156+nikolat@users.noreply.github.com>
2024-08-05 21:06:52 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,182 @@
+import * as nip19 from 'nostr-tools/nip19';
+import { finalizeEvent, NostrEvent } from 'nostr-tools/pure';
+import {
+  SimplePool,
+  useWebSocketImplementation,
+  type SubCloser,
+} from 'nostr-tools/pool';
+import WebSocket from 'ws';
+import { Filter } from 'nostr-tools/filter';
+useWebSocketImplementation(WebSocket);
+
+const isDebug = false;
+
+(async () => {
+  const relaysToFetch = [
+    'wss://relay.nostr.band/',
+    'wss://nos.lol/',
+    'wss://relay.damus.io/',
+    'wss://relay.nostr.wirednet.jp/',
+    'wss://relay-jp.nostr.wirednet.jp/',
+    'wss://yabu.me/',
+    'wss://r.kojira.io/',
+    'wss://relay.mymt.casa/',
+  ];
+
+  const relaysToWrite = [
+    'wss://relay.nostr.wirednet.jp/',
+    'wss://relay.mymt.casa/',
+  ];
+
+  const now = new Date();
+  const until =
+    Math.floor(
+      new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime() /
+        1000,
+    ) -
+    9 * 60 * 60;
+  const since = until - 24 * 60 * 60;
+
+  const getGeneralEvents = (
+    pool: SimplePool,
+    relays: string[],
+    filters: Filter[],
+    callbackEvent: Function = () => {},
+    autoClose: boolean = true,
+  ): Promise<NostrEvent[]> => {
+    return new Promise((resolve) => {
+      const events: NostrEvent[] = [];
+      const onevent = (ev: NostrEvent) => {
+        events.push(ev);
+        callbackEvent(ev);
+      };
+      const oneose = () => {
+        if (autoClose) {
+          sub.close();
+          pool.close(relays);
+        }
+        resolve(events);
+      };
+      const sub: SubCloser = pool.subscribeMany(relays, filters, {
+        onevent,
+        oneose,
+      });
+    });
+  };
+
+  const getReactions = async (
+    pool: SimplePool,
+    relays: string[],
+  ): Promise<NostrEvent[]> => {
+    const reactionEventsFetched = await getGeneralEvents(pool, relays, [
+      { kinds: [17], since, until },
+    ]);
+    return reactionEventsFetched;
+  };
+
+  const postNostr = async (
+    pool: SimplePool,
+    sk: Uint8Array,
+    message: string,
+    relays: string[],
+    urls: string[],
+    hashTag: string,
+  ) => {
+    const tags = [['t', hashTag], ...urls.map((url) => ['r', url])];
+    const unsignedEvent = {
+      kind: 1,
+      created_at: Math.floor(Date.now() / 1000),
+      tags: tags,
+      content: message,
+    };
+    const signedEvent = finalizeEvent(unsignedEvent, sk);
+    const pubs = pool.publish(relays, signedEvent);
+    await Promise.any(pubs);
+    pool.close(relays);
+  };
+
+  const rankingEmoji = new Map<Number, string>([
+    [1, '🥇'],
+    [2, '🥈'],
+    [3, '🥉'],
+    [4, '④'],
+    [5, '⑤'],
+    [6, '⑥'],
+    [7, '⑦'],
+    [8, '⑧'],
+    [9, '⑨'],
+    [10, '⑩'],
+    [11, '⑪'],
+    [12, '⑫'],
+    [13, '⑬'],
+    [14, '⑭'],
+    [15, '⑮'],
+    [16, '⑯'],
+    [17, '⑰'],
+    [18, '⑱'],
+    [19, '⑲'],
+    [20, '⑳'],
+  ]);
+
+  const main = async () => {
+    const NOSTR_PRIVATE_KEY = process.env.NOSTR_PRIVATE_KEY ?? '';
+    const pool = new SimplePool();
+    const events = await getReactions(pool, relaysToFetch);
+    const urls = events
+      .map((ev) =>
+        ev.tags
+          .find(
+            (tag) => tag.length >= 2 && tag[0] === 'r' && URL.canParse(tag[1]),
+          )
+          ?.at(1),
+      )
+      .filter((ev) => ev !== undefined);
+    const ranking = new Map<string, number>();
+    for (const url of urls) {
+      if (ranking.has(url)) {
+        ranking.set(url, ranking.get(url)! + 1);
+      } else {
+        ranking.set(url, 1);
+      }
+    }
+    const hashtag = 'makibishiranking';
+    let message = `${new Date(since * 1000).toLocaleDateString('ja-JP')}のまきびしランキング #${hashtag}\n\n`;
+    const urlsSorted = Array.from(ranking.keys());
+    urlsSorted.sort((a, b) => {
+      const m = ranking.get(a)!;
+      const n = ranking.get(b)!;
+      return n - m;
+    });
+    let count_rank = 1;
+    let count_url = -1;
+    let index = 0;
+    for (const url of urlsSorted) {
+      let rank;
+      if (ranking.get(url) === count_url) {
+        rank = count_rank;
+      } else {
+        count_rank = index + 1;
+        rank = count_rank;
+        count_url = ranking.get(url)!;
+      }
+      index++;
+      message += `${rankingEmoji.get(rank)} ${ranking.get(url)} ${url}\n`;
+      if (index >= 19) break;
+    }
+    if (!isDebug) {
+      const { type, data } = nip19.decode(NOSTR_PRIVATE_KEY);
+      if (type !== 'nsec') {
+        console.warn('NOSTR_PRIVATE_KEY is not nsec');
+        return;
+      }
+      const sk: Uint8Array = data;
+      await postNostr(pool, sk, message, relaysToWrite, urlsSorted, hashtag);
+    } else {
+      console.log('message: ', message);
+    }
+    console.log('post complete');
+    process.exit(0);
+  };
+
+  await main();
+})();
