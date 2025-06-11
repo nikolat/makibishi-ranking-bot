423b609165734220bca971a145d8270e89802eeb
Don <496156+nikolat@users.noreply.github.com>
2025-06-12 06:25:11 +0900

    エラーログを表示

--- src/main.ts
+++ src/main.ts
@@ -147,5 +147,10 @@ const isDebug = false;
     process.exit(0);
   };
 
-  await main();
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 })();
